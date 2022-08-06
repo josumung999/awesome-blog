c5a175898b63476582ecaec930e5054d8a113438
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-06 16:01:50 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from "reactstrap"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button color='danger'>Danger !</Button>
+    </div>
+  );
+}
+
+export default App;
