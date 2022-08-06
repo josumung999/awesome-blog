2367595e2b4e6ff9aac4666aca07dcdedbf55d6c
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-06 23:46:18 +0200

    Built the basic UI for postlist

--- src/components/posts/PostItem.jsx
+++ src/components/posts/PostItem.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Card, CardBody, CardText, CardTitle } from 'reactstrap'
 
 const PostItem = (props) => {
   return (
-    <div>
-      {props.post.title}
-    </div>
+    <Card className='my-2'>
+      <CardBody>
+        <CardTitle tag="h5">
+          {props.post.title}
+        </CardTitle>
+        <CardText>
+          <small className='text-muted'>
+            {props.post.date}
+          </small>
+        </CardText>
+        <Link to={`/post/${props.post.id}`}>
+          <Button color='primary'>
+            Read More
+          </Button>
+        </Link>
+      </CardBody>
+    </Card>
   )
 }
 
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -6,7 +6,7 @@ import PostItem from './PostItem';
 const Posts = () => {
   return (
     <Container style={{ paddingTop: "70px" }}>
-      Posts
+      <h2 className='my-4'>Latest Posts</h2>
       <List>
         {data.posts.map((post) => (
           <PostItem post={post} key={post.id} />
