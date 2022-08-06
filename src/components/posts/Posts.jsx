3b35cbaae413d7c0304f908a6032749e58249abe
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-07 00:08:29 +0200

    Revert "Tried unsuccessfully setting up pagination"
    This reverts commit 71a0ef3d3264fb7886fa0f88a7c5d4a453d2c4f2.

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -3,35 +3,15 @@ import { Container, List } from 'reactstrap'
 import data from '../../data/posts.json';
 import PostItem from './PostItem';
 
-// setting up pagination
-import ReactPaginate from 'react-paginate';
-import { useState } from 'react';
-import { useEffect } from 'react';
-
-const Posts = ({itemsPerPage}) => {
-  const [items, setItems] = useState([]);
-  const [pageCount, setPageCount] = useState(0);
-
-  let limit = 3;
-
-
+const Posts = () => {
   return (
     <Container style={{ paddingTop: "70px" }}>
       <h2 className='my-4'>Latest Posts</h2>
       <List>
-        {currentItems.map((post) => (
+        {data.posts.map((post) => (
           <PostItem post={post} key={post.id} />
         ))}
       </List>
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel="next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="< previous"
-        renderOnZeroPageCount={null}
-      />
     </Container>
   )
 }
