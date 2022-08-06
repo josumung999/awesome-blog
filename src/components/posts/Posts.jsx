8832d8bcecb1b2546d5de731358dce13bd4b6158
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2022-08-06 16:51:14 +0200

    Fetching posts from json

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Container } from 'reactstrap'
+import { Container, List } from 'reactstrap'
+import data from '../../data/posts.json';
 
 const Posts = () => {
   return (
     <Container style={{ paddingTop: "70px" }}>
       Posts
+      <List>
+        {data.posts.map((post) => (
+          <li>
+            {post.title}
+          </li>
+        ))}
+      </List>
     </Container>
   )
 }
